8da0cd5d40288f2677b9b717497414b47c7a2a7c
guillenjs <jorgeguilln@gmail.com>
2020-11-28 11:00:45 -0500

    movies can be liked and disliked with information persisting on backend and front-end

--- src/components/FilmView.js
+++ src/components/FilmView.js
@@ -106,14 +106,24 @@ const  API_KEY = process.env.REACT_APP_FILM_REEL_API_KEY
 function FilmView(props) {
     const [filmInfo, setFilmInfo] = useState({})
     const [allGenres, setGenres] = useState({})
-    // const {title} = filmInfo
-
-    // console.log(title)
-    const{title, backdrop_path, poster_path, release_date, overview, genre_ids, id} = props.currentFilmClick
-
-    //everytime object is clicked on list it will render all info will be fetched from API]
-
+    const [likes, setLikes] = useState(0)
+    const [disLikes, setDisLikes] = useState(0)
+   
     const fetchInfo = () => {
+        //check if like or dislike exists
+        fetch(`http://localhost:3000/movies/${filmInfo.id}`)
+        .then(res => res.json())
+        .then(film => {
+            console.log(film)
+            if(film.error !== true)
+            {
+                setLikes(film.likes)
+                setDisLikes(film.dislikes)
+            }
+           
+        })
+
+        //fetch movie information
         fetch(`https://api.themoviedb.org/3/movie/${props.currentFilmClick.id}?api_key=${API_KEY}&language=en-US`)
         .then(res => res.json())
         .then(film => {
@@ -125,24 +135,46 @@ function FilmView(props) {
         })
     }
 
+
     useEffect(() => {
         fetchInfo()
     },[filmInfo.id]
     )
 
-    
+    //add like and dislike to rails api
+    const handleLike = () => {
+        fetch(`http://localhost:3000/movies/${filmInfo.id}/add_like`,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }   
+        )
+        .then(res => res.json())
+        .then(obj => setLikes(obj.likes))
+    }
 
-    // do fetch for backend with current film info to rails api
-    //if found then set state for like and dislikes 
-    // if not found then render the likes and dislikes as 0 
+    const handleDisLike = () => {
+        fetch(`http://localhost:3000/movies/${filmInfo.id}/add_dislike`,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }   
+        )
+        .then(res => res.json())
+        .then(obj => setDisLikes(obj.dislikes))
+    }
+
 
 console.log(filmInfo)
- 
+ console.log(likes)
     return (
         <StyledDiv>
             <StyledContent
              style={filmInfo.backdrop_path? 
-                            {backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)), url("https://image.tmdb.org/t/p/original${backdrop_path}")`}: 
+                            {backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)), 
+                                                url("https://image.tmdb.org/t/p/original${filmInfo.backdrop_path}")`}: 
                             {backgroundColor: 'lightgrey'}}
                             >
 
@@ -152,14 +184,15 @@ console.log(filmInfo)
                 </StyledHeader>
                     
                 <StyledMovie>
-                    <img src={`https://image.tmdb.org/t/p/original${filmInfo.poster_path}`}></img>
+                    <img src={`https://image.tmdb.org/t/p/original${filmInfo.poster_path}`} alt="poster"></img>
                     <div>
                         <h2>{filmInfo.title}</h2>
                             <p><h4>{filmInfo.release_date}</h4></p>
                             <p><h4>{Object.keys(allGenres).length !== 0? `${allGenres.name}`: ''}</h4></p>
                             <p><h4>{filmInfo.runtime} min</h4></p>
                             <p>{filmInfo.tagline}</p>
-                        82<i className="fa fa-thumbs-up"></i> 5<i className="fa fa-thumbs-down"></i>
+                                {likes}<i className="fa fa-thumbs-up" onClick={handleLike}></i> 
+                                {disLikes}<i className="fa fa-thumbs-down" onClick={handleDisLike}></i>
                     </div>
                 </StyledMovie>
 
